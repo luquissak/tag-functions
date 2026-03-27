ab4c1e82a8a96fce660baea7dc4dafcd0037c205
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2026-03-27 15:44:13 -0300

    deploy verificar_login ok

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # tag-functions
+
 functions firebase
 
-
-
 # Setup Firebase Functions
+
 ```bash
 firebase init functions
 cd functions
@@ -13,8 +13,32 @@ python -m pip install --upgrade pip
 python -c "import firebase_functions; print('Firebase pronto!')"
 ```
 
+# Run locally
 
 ```bash
 Stop-Process -Id (Get-NetTCPConnection -LocalPort 4000).OwningProcess -Force
 firebase emulators:start --only functions
 ```
+
+# Teste
+
+```bash
+$url = "http://127.0.0.1:5001/llm-studies/us-central1/verificar_login"
+$url_nuvem = "https://verificar-login-3hqo37xxza-uc.a.run.app"
+$body = @{ email = "lquissakng@gmail.com" } | ConvertTo-Json
+Invoke-RestMethod -Uri $url -Method Post -ContentType "application/json" -Body $body
+Invoke-RestMethod -Uri $url_nuvem -Method Post -ContentType "application/json" -Body $body
+
+```
+
+# Deploy
+
+```bash
+functions\venv\Scripts\activate
+pip freeze > .\functions\requirements.txt
+firebase deploy --only functions
+```
+
+# Links
+
+- [Cloud Firestore](https://console.firebase.google.com/project/llm-studies/firestore/databases/-default-/data/~2Fusuarios_autorizados)
--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,52 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
-from firebase_functions.options import set_global_options
-from firebase_admin import initialize_app
+from firebase_admin import initialize_app, firestore
+import json
 
-# For cost control, you can set the maximum number of containers that can be
-# running at the same time. This helps mitigate the impact of unexpected
-# traffic spikes by instead downgrading performance. This limit is a per-function
-# limit. You can override the limit for each function using the max_instances
-# parameter in the decorator, e.g. @https_fn.on_request(max_instances=5).
-set_global_options(max_instances=10)
+initialize_app()
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@https_fn.on_request()
+def verificar_login(req: https_fn.Request) -> https_fn.Response:
+    # 1. Tratamento de CORS para chamadas do Frontend
+    if req.method == "OPTIONS":
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type",
+        }
+        return https_fn.Response("", status=204, headers=headers)
+
+    headers = {"Access-Control-Allow-Origin": "*"}
+
+    if req.headers.get("X-Firebase-AppCheck") is None:
+            return https_fn.Response("Não autorizado pelo App Check", status=401)
+
+    try:
+        data = req.get_json()
+        email_fornecido = data.get("email")
+
+        if not email_fornecido:
+            return https_fn.Response("Email ausente", status=400, headers=headers)
+
+        db = firestore.client()
+        # Busca o email na coleção do Firestore
+        docs = db.collection("usuarios_autorizados").where("email", "==", email_fornecido).stream()
+        authorized = any(docs)
+
+        log_entry = {
+            "email": email_fornecido,
+            "status": "sucesso" if authorized else "falha",
+            "timestamp": firestore.SERVER_TIMESTAMP,
+#            "ip": req.remote_addr, # Pega o IP do cliente
+            "ip": req.headers.get('X-Forwarded-For', req.remote_addr),
+            "user_agent": req.headers.get("User-Agent")
+        }
+        db.collection("audit_logs").add(log_entry)
+
+        if authorized:
+            return https_fn.Response(json.dumps({"auth": True}), status=200, headers=headers, mimetype="application/json")
+        else:
+            return https_fn.Response(json.dumps({"auth": False}), status=403, headers=headers, mimetype="application/json")
+
+    except Exception as e:
+        db.collection("audit_logs").add({"status": "erro_sistema", "erro": str(e), "timestamp": firestore.SERVER_TIMESTAMP})
+        return https_fn.Response(str(e), status=500, headers=headers)
